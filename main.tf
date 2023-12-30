4496381e12f26605d7766703ab0a68a694e5dc6e
jesni <jesni@gmail.com>
2023-12-30 05:09:28 +0000

    initial commit

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "http_access" {
 #Ec2 instance creation
 
 resource "aws_instance" "frontend" {
-  ami                    = data.aws_ami.latest.id
+  ami                    = var.instance_ami
   instance_type          = var.instance_type
   key_name               = aws_key_pair.mykey.key_name
   vpc_security_group_ids = [aws_security_group.http_access.id]
