ecd5c3ccf9565aad010dc1839ec485e270826b13
jesni <jesni@gmail.com>
2023-12-30 05:35:27 +0000

    ftp port

--- main.tf
+++ main.tf
@@ -37,7 +37,14 @@ resource "aws_security_group" "http_access" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
- ingress {
+ingress {
+    from_port        = 21
+    to_port          = 21
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  } 
+ingress {
     from_port        = 9090
     to_port          = 9090
     protocol         = "tcp"
